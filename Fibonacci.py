572edb336993ca49712dd8d6cf203ca3c0f3e493
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2021-12-22 10:57:20 +0200

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,29 @@
+def fibonacci(xterms):
+    # первые два условия
+    x1 = 0
+    x2 = 1
+    count = 0
+
+    if xterms <= 0:
+        print("Укажите целое число больше 0")
+    elif xterms == 1:
+        print("Последовательность Фибоначчи до", xterms, ":")
+        print(x1)
+    else:
+        while count < xterms:
+            xth = x1 + x2
+            x1 = x2
+            x2 = xth
+            count += 1
+            yield xth
+
+
+fib = fibonacci(int(input('Количество чисел фибоначи: ')))
+
+# print(next(fib))
+# print(next(fib))
+# print(next(fib))
+# print(next(fib))
+# print(next(fib))
+# print(next(fib))
+print(*fib)
